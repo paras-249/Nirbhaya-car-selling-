65fa015fec390a0165fd122e78e629b93a25abcf
PARAS <doyeparas196@gmail.com>
2025-01-03 15:43:03 +0530

    processing

---
+++ Home/home.js
@@ -0,0 +1,9 @@
+
+  function contact(){
+    let c_list=document.querySelector("#search");
+    c_list=[
+      " Mb. No: +91 8459536715" ,"                                                       ",            
+      " Email Address: doyeparas196@gamil.com"
+    ]
+    alert(c_list);
+  }
